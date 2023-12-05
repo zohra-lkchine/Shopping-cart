380ad12a07fc166b9f285ef738054d8450f52891
zohra-lkchine <zohralkchine@gmail.com>
2023-12-05 17:47:31 +0100

    Create Like button and change color funtion

--- script.js
+++ script.js
@@ -61,7 +61,16 @@ const Addproducts = () => {
       return `
       <div class="item">
       <img src="${item.image}" alt="${item.name}" class="image">
+      <div class="like">
       <h3>${item.name}</h3>
+      <button class="likebtn" id="#like-${item.id}" onclick="likeItem(${item.id})">
+      <svg xmlns="http://www.w3.org/2000/svg" class="likeicon" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+</svg>
+
+
+      </buton>
+      </div>
       <span>${item.price} $</span>
       <button type="button" onclick="getItem(${item.id})" class="additem">Add to Cart</button>
       <div class="addtocart">
@@ -91,7 +100,6 @@ let currentProduct = products.find(item => item.id === id);
 
 let numbrElement = document.getElementById(`#product-${currentProduct.id}`);
     
-console.log(numbrElement);
 
      cartdiv.innerHTML += `
     <div class="cart-items">
@@ -110,11 +118,35 @@ console.log(numbrElement);
     
 }
 
+// Like function 
+
+const likeItem = (id) => {
+    let currentLiked = products.find(item => item.id === id);
+    
+    let likeIcon = document.getElementById(`#like-${currentLiked.id}`);
+
+    likeIcon.style.color == "" ? likeIcon.style.color = "red" : likeIcon.style.color = "" 
+
+    
+
+
+
+
+}
+
+
+
+
+
 // function pour supprimer touts les produits du panier
 
 const removeAll = () => {
     cartdiv.innerHTML = "";
     totalpDiv.innerHTML = "";
+
+    setTimeout(() => {
+        location.reload();
+    }, 500);
 }
 
 
