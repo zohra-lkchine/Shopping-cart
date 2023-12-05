561efa782cf24b16c1d48776b068ad6b53fb7b8f
zohra-lkchine <zohralkchine@gmail.com>
2023-12-05 17:13:43 +0100

    update item number in cart

--- script.js
+++ script.js
@@ -3,37 +3,43 @@ let products = [
         id: 1,
         name: "Burger",
         price: 200,
-        image: "assets/burger.png"
+        image: "assets/burger.png",
+        numbrInCart: 0
     },
     {
         id: 2,
         name: "Roast chiken",
         price: 200,
-        image: "assets/chicken.jpg"
+        image: "assets/chicken.jpg",
+        numbrInCart: 0
     },
     {
         id: 3,
         name: "Pasta",
         price: 1200,
-        image: "assets/pasta.jpg"
+        image: "assets/pasta.jpg",
+        numbrInCart: 0
     },
     {
         id: 4,
         name: "Pizza",
         price: 600,
-        image: "assets/pizza.jpeg"
+        image: "assets/pizza.jpeg",
+        numbrInCart: 0
     },
     {
         id: 5,
         name: "Salade",
         price: 250,
-        image: "assets/salade.jpg"
+        image: "assets/salade.jpg",
+        numbrInCart: 0
     },
     {
         id: 6,
         name: "Soap",
         price: 180,
-        image: "assets/soupe.jpg"
+        image: "assets/soupe.jpg",
+        numbrInCart: 0
     },
 ];
 
@@ -43,33 +49,50 @@ let cartdiv = document.querySelector(".cart .addeditems");
 let addeditems = document.querySelectorAll(".cart .addeditems .cart-items");
 let totalpDiv = document.querySelector(".totalprice");
 let deleteBtn = document.querySelector(".delete");
+let numbrAdd = document.querySelector(".nbr");
 let totalPrice = 0;
+
+
+
+
 
 const Addproducts = () => {
     let productsList = products.map(item => {
       return `
-        <div class="item">
-            <img src="${item.image}" alt="${item.name}" class="image">
-            <h3>${item.name}</h3>
-            <span>${item.price} $</span>
-            <button type="button" onclick="getItem(${item.id})" class="additem">Add to Cart</button>
-            <div class="addtocart">
-                <span class="plus">+</span>
-                <span class="number">0</span>
-                <span class="moins">-</span>
-            </div>
-        </div>
-        `
+      <div class="item">
+      <img src="${item.image}" alt="${item.name}" class="image">
+      <h3>${item.name}</h3>
+      <span>${item.price} $</span>
+      <button type="button" onclick="getItem(${item.id})" class="additem">Add to Cart</button>
+      <div class="addtocart">
+      <span class="plus">+</span>
+      <span class="nbr" id="#product-${item.id}">${item.numbrInCart}</span>
+      <span class="moins">-</span>
+      </div>
+      </div>
+      `
     })
     
     layoutdiv.innerHTML = productsList.join('');
 }
 
+// function call
+Addproducts();
+
+
+let newNmb = 0;
+
+
 // function pour ajouter produit au panier
 const getItem = (id) => {
-let currentProduct = products.find(item => item.id === id);
     
-console.log(currentProduct)
+
+let currentProduct = products.find(item => item.id === id);
+
+let numbrElement = document.getElementById(`#product-${currentProduct.id}`);
+    
+console.log(numbrElement);
+
      cartdiv.innerHTML += `
     <div class="cart-items">
     <img src="${currentProduct.image}" width="100" height="100" />
@@ -82,6 +105,9 @@ console.log(currentProduct)
     totalPrice += currentProduct.price;
     totalpDiv.innerHTML = `${totalPrice} $`;
 
+    currentProduct.numbrInCart = newNmb + 1;
+    numbrElement.innerHTML = `<span>${currentProduct.numbrInCart}</span>`
+    
 }
 
 // function pour supprimer touts les produits du panier
@@ -92,4 +118,3 @@ const removeAll = () => {
 }
 
 
-Addproducts();
